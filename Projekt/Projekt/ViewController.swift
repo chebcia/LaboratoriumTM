77bdb051217c7e8ade7720ff9389838caa7f56fd
Natalia Cheba <43813992+chebcia@users.noreply.github.com>
2020-10-25 21:48:09 +0100

    Update ViewController.swift

--- Projekt/Projekt/ViewController.swift
+++ Projekt/Projekt/ViewController.swift
@@ -1,84 +1,52 @@
-//
-//  ViewController.swift
-//  Projekt
-//
-//  Created by Student on 19/10/2020.
-//  Copyright © 2020 Łukasz. All rights reserved.
-//
-
 import UIKit
 
-import Foundation
-
-
 class ViewController: UIViewController {
-   
+    
     var punkty = 0
-    var runda = 1
-    var losowanie = -1
-    let wynik = Int.random(in: 1...100)
-    var counter = 0
-    func policz_punkty()
+    var runda = 0
+    var wynik = Int.random(in: 1...100)
+    var liczba = 0
+
+    @IBOutlet weak var sliderek: UISlider!
+    @IBOutlet weak var round: UILabel!
+    @IBOutlet weak var RandomNumber: UILabel!
+    @IBOutlet weak var point: UILabel!
+    @IBAction func check(_ sender: Any)
     {
-        let wybieranie = slider.value
-        if wynik == losowanie
-        {
-            punkty = punkty + 100
-        }
-        else if abs(wynik - losowanie) == 1
-        {
-            punkty = punkty + 99
-        }
-        else if abs(wynik - losowanie) == 15
-        {     punkty = punkty + 85
-        }
-        else if abs(wynik - losowanie) == 25
-        {    punkty = punkty + 75
+        if(runda < 10)
+        { runda = runda+1
+            if(wynik == liczba)
+            {
+                punkty = punkty + 100
+            }
+            else if (abs(wynik - liczba)) == 10
+            {
+                punkty = punkty + 99
+            }
+            else if (abs(wynik - liczba)) == 15
+            {
+                punkty = punkty + 85
+            }
+            else if (abs(wynik - liczba)) == 25
+            {
+                punkty = punkty + 75
+            }
+            point.text = (String)(punkty)
+            round.text = (String)(runda)
+            RandomNumber.text = (String)(wynik)
         }
     }
-    
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var sliderVal: UILabel!
-    
-   
-    @IBOutlet weak var sprawdzbutton: UIButton!
-    
-    @IBAction func Sprawdź(_ sender: Any) {
-        if(counter==10)
-        {
-           // resetowanie wyniku
-            
+    @IBAction func Restart_button(_ sender: Any) {
+    RandomNumber.text = (String)(wynik)
+        runda = 1
+        punkty = 0
         
-        }
-        else{
-            
-             policz_punkty()
-        }
     }
-    
-    func graj()
-    {
-        
-        Sprawdź(<#T##sender: Any##Any#>)
+    @IBAction func slider(_ sender: Any) {
+        liczba = Int(sliderek.value)
     }
-    
-    
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
     }
-
-
 }
